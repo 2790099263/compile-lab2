64f466c091f42d29ef71dc183dd27b7672264afa
lsc <2790099263@qq.com>
2022-12-02 02:06:09 +0800

    update list struct test

--- list.c
+++ list.c
@@ -1,3 +1,7 @@
+/*
+list struct test program;
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 typedef struct list_node
@@ -7,7 +11,8 @@ typedef struct list_node
     struct list *lst;
     struct list *nxt;
 }list;
-list* listInit(list *lst_node){
+list *head;
+list* listInit(){
     list *l;
     l=(list*)malloc(sizeof(list));
     if(l==NULL){
@@ -15,31 +20,38 @@ list* listInit(list *lst_node){
         exit(0);
     }
     l->nxt=NULL;
-    l->lst=lst_node;
+    l->lst=NULL;
     return l;
 }
-void listPush(int v,list *now){
+void listPush(list *now,int v){
     list *nx;
     list *po=now;
     while(po->nxt!=NULL)po=po->nxt;
-    nx = listInit(po);
+    nx = (list*)malloc(sizeof(list));
+    if(nx == NULL){
+        printf("ERROR! NO MEMORY!\n");
+        exit(0);
+    }
     nx->val=v;
     nx->lst=po;
     po->nxt=nx;
     return ;
 }
 int main(){
-    list *bga,*end;
-    bga = listInit(NULL);
+    head = listInit();
     for(int i=1;i<=10;i++)
     {
         int a=0;
         scanf("%d",&a);
-        listPush(a,bga);
+        listPush(head,a);
     }
-    list *po = bga;
-    while(po->nxt!=NULL){
-        po=po->nxt;
-        printf("%d\n",po->val);
+    list *lin;
+    lin=head;
+    while (lin->nxt!=NULL)
+    {
+        lin=lin->nxt;
+        printf("%d\n",lin->val);
     }
+    
+
 }
