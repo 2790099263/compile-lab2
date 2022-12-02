dc9f8316f41926c29c615f26245e8e4bf6a1fd59
lsc <2790099263@qq.com>
2022-12-02 08:42:32 +0800

    update Clang_make file

--- Clang_make.sh
+++ Clang_make.sh
@@ -1,3 +1,5 @@
-echo "即将输出文件$1的clang编译ast:"
 out=${1#*es/}
-`clang  -fsyntax-only -Xclang -ast-dump $1 | sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g" > ./out_cases/${out%.c*}.txt`
+echo "[+]Clang Compile file: ./out_cases/${out%.c*}.txt"
+`clang  -fsyntax-only -Xclang -ast-dump $1 | \
+sed -r "s/\x1B\[([0-9]{1,3}((;[0-9]{1,3})*)?)?[m|K]//g"  \
+> ./out_cases/${out%.c*}.txt`
